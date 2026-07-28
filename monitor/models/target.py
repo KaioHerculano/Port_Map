0d08373c62aee8788a7cd966de2346779092835e
Kaio Herculano <kaioherculano12@gmail.com>
2026-07-27 23:40:17 -0400

    feat: add company relation to target model

--- monitor/models/target.py
+++ monitor/models/target.py
@@ -10,6 +10,9 @@ from .validators import validate_host
 
 
 class MonitorTarget(models.Model):
+    company = models.ForeignKey(
+        "accounts.Company", on_delete=models.CASCADE, related_name="targets"
+    )
     device = models.ForeignKey(
         Device, on_delete=models.CASCADE, null=True, blank=True, related_name="sensors"
     )
