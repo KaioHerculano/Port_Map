880a934da44859b765750a953365f27f72d70538
Kaio Herculano <kaioherculano12@gmail.com>
2026-07-27 23:38:43 -0400

    feat: create company model and link to user

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    document = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.SET_NULL, null=True, blank=True, related_name="users"
+    )
 
     def __str__(self):
         return self.username
